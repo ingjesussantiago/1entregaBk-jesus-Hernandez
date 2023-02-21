5881d9ac28a5d0a84e1ec2fb335103728f556dfc
Jesus Santiago Hernandez Lopez <ingjesussantiago@gmail.com>
2023-02-20 19:43:06 -0600

    avance

--- products.js
+++ products.js
@@ -8,57 +8,74 @@ getproducto(){
     return this.producto
 }
 
+agregarProducto(nombre,descripcion,image, precio,code,categoria,stock=30){
+
+
+  
+  
+    
+        
+        const producto={
+            id:this.#agregarId(),
+            nombre,
+            descripcion,
+            image,
+            precio,
+            code,
+            categoria,
+            stock,
+        }
+
+        
+        this.producto.push(producto)
+        console.log(producto);
+        this.#verificaCode()
+     
+       
+     }
+
 #agregarId(){
     
     const id=this.producto.length ===0
-    ?1
-    :this.producto[this.producto.length-1].id+1
+     ?1
+    :this.producto[this.producto.length-1].id+1 
     return id
+    
 }
+
 
 #verificaCode(){
-    const code =this.code
-    const codeVerifica = this.producto.find(producto=>producto.code===code)
-    ?code
-    :this.code
-    return code
+
+ const code =this.producto.some(element =>element.code ===this.producto.code)
+    code
+    ? console.log("repetido")
+    : this.code
+    
+   
+// if (this.producto.some(element =>element.code ===this.producto.code) ){
+//     console.log('Error: El código del producto ya existe')
+//     return;
+// }
+
 
 }
 
-getproductoById(){
-
-}
-
-
-agregarProducto(nombre,descripcion="n/a",image="n/a", precio,code,categoria,stock=30){
-
-    const producto={
-        id:this.#agregarId(),
-        nombre,
-        descripcion,
-        image,
-        precio,
-        code:this.#verificaCode(),
-        categoria,
-        stock,
+getProductoById(id) {
+    const producto = this.products.find((p) => p.id === id);
+    if (producto) {
+        return producto;
+    } else {
+        console.log('Error: Not found');
     }
-    this.producto.push(producto)
-    console.log(producto)
- }
+}
 
- 
 
 }
 const manager = new Tienda();
-manager.agregarProducto("camisa","algodo",1,30,001,"ropa")
-manager.agregarProducto("cadena","oro 18k",2,300,002,"joyas")
-manager.agregarProducto("anillo","oro 24k", 3,400,003,"joyas")
-// manager.agregarProducto("pulsera","oro 24k", 3,500,003,"joyas")
-
-
-
-
-
+manager.agregarProducto("camisa","algodo",1,30, 1,"ropa")
+manager.agregarProducto("cadena","oro 18k",2,300,2,"joyas")
+manager.agregarProducto("anillo","oro 24k", 3,400,3,"joyas")
+manager.agregarProducto("pulsera","oro 24k", 4,500,3,"joyas")
 
 
 
